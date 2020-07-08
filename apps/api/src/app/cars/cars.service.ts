574de9364e54410df665dd7a859fc9fe141278f0
Leonardo Victor <leonardovff@gmail.com>
2020-07-08 18:08:10 -0300

    add a seed base in cars

--- apps/api/src/app/cars/cars.service.ts
+++ apps/api/src/app/cars/cars.service.ts
@@ -3,7 +3,7 @@ import { Car } from '@testetinnova/api-interfaces';
 
 @Injectable()
 export class CarsService {
-  private readonly cars: Car[] = [];
+  private readonly cars: Car[] = [{"veiculo":"Audioooo","marca":"Dongfeng","ano":2010,"descricao":"testeee","vendido":true,"created":"2010-05-02 12:12:11","updated":"2010-05-02 12:12:11","id":2},{"veiculo":"Audioooo","marca":"Bentley","ano":2010,"descricao":"teste testee","vendido":true,"created":"2010-05-02 12:12:11","updated":"2010-05-02 12:12:11","id":3},{"veiculo":"Teste 2","marca":"Audi","ano":1991,"descricao":"teste","vendido":true,"id":4},{"veiculo":"asdasd","marca":"Aston Martin","ano":1992,"descricao":"testee","vendido":false,"id":12},{"veiculo":"testeeee","marca":"Audi","ano":1992,"descricao":"testeee","vendido":true,"id":13},{"veiculo":"teste 6","marca":"Agrale","ano":1993,"descricao":"testee 4","vendido":true,"id":14}];
   private count = 0;
 
   findAll(): Car[] {
